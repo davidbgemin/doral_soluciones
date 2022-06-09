8fc08f237106f0c8535d0a49d5e6d8e11bfa849f
david <davidbgemin@gmail.com>
2022-06-09 18:19:05 -0500

    reserva

--- js/eventos.js
+++ js/eventos.js
@@ -1,13 +1,13 @@
-function print(ruta) {
-    Swal.fire({
-        // title: '',
-        // showCloseButton: true,
-        showConfirmButton:false,
-        // text: '',
-        padding: '1em',
-        imageUrl: ruta,
-        imageWidth: 900,
-        imageHeight: 600,
-        imageAlt: '',
-      })
+function mostrarFoto(ruta) {
+  Swal.fire({
+      // title: '',
+      // showCloseButton: true,
+      showConfirmButton:false,
+      // text: '',
+      padding: '1em',
+      imageUrl: ruta,
+      imageWidth: 900,
+      imageHeight: 600,
+      imageAlt: '',
+    })
 }
